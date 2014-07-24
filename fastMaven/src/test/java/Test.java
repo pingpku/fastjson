fb72353fd578a946a422a4addaa0da66db41e6a6
pingpku <pingpku@163.com>
2014-07-24 11:30:52 +0800

    tst

---
+++ fastMaven/src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: guoping
+ * Date: 14-7-24
+ * Time: 上午11:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("test git");
+    }
+
+}
